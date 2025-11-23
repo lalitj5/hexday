026bd468649e647dc25b900b658d69caf8c06d32
Kevin Xiang <kxiang644@gmail.com>
2025-11-23 12:27:12 -0500

    Adding starter database functions generate_id(), create_new_user()

---
+++ backend/database.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+import uuid
+
+# Replace with your connection string
+uri = "mongodb+srv://kxiang644:HexDays@cluster0.cpcw8iv.mongodb.net/"
+client = MongoClient(uri)
+
+# Access a database
+db = client["vacation_scrapbook"]
+
+# Access a collection
+user_collection = db["users"]
+trip_collection = db["trips"]
+media_collection = db["media"]
+
+def generate_id():
+    return str(uuid.uuid4())
+
+def create_new_user(username): 
+    # Check that the user does not exist
+    if user_collection.find_one({"username": username}):
+        raise Exception("User has been taken!")
+    user_collection.insert_one(
+        {
+            "username": username,
+            "user_id": generate_id(),
+            "profile_photo_id": 0,
+        }
+
+
+    )
+
+print("Connected!") 
